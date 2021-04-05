cb8c4be353c1e8ae34ec80dfc44a143872e802d2
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-04-05 08:18:23 -0500

    New starter code was released.

--- p3/src/MyBadDatagramSocketP3.java
+++ p3/src/MyBadDatagramSocketP3.java
@@ -15,45 +15,45 @@ public class MyBadDatagramSocketP3 extends DatagramSocket {
     private final int MINIMUM_ONE_OUT_OF = 2;
     private int corruptOneOutOf = DEFAULT_ONE_OUT_OF;
     private Random rand = new Random();
-
+    
     public MyBadDatagramSocketP3() throws SocketException {
         super();
     }
-
+    
     public MyBadDatagramSocketP3(int inPort) throws SocketException {
         super(inPort);
     }
-
+    
     public void setDropOneOutOf(int inDropOneOutOf) {
         if (inDropOneOutOf < MINIMUM_ONE_OUT_OF)
             throw new IllegalArgumentException();
     }
-
+    
     public void setCorruptOneOutOf(int inCorruptOneOutOf) {
         if (inCorruptOneOutOf < MINIMUM_ONE_OUT_OF)
             throw new IllegalArgumentException();
     }
-
+   
     @Override
     public void send(DatagramPacket inPkt) throws IOException {
         DatagramPacket sendPkt = inPkt;
-        if (corruptOneOutOf > 0 &&
-            inPkt.getLength() > 0 &&
+        if (corruptOneOutOf > 0 && 
+            inPkt.getLength() > 0 && 
             rand.nextInt(corruptOneOutOf) == 0) {
             int len = inPkt.getLength();
             // Copy the data so the client doesn't see the corruption
             byte[] corruptBytes = new byte[len],
                    inPktBytes = inPkt.getData();
-            System.arraycopy(inPktBytes,
-                             0,
-                             corruptBytes,
-                             0,
+            System.arraycopy(inPktBytes, 
+                             0, 
+                             corruptBytes, 
+                             0, 
                              len);
             /*
             Flip exactly one random bit...
             */
             int rbyte = rand.nextInt(len), // a random byte
-                rbit = 1 << rand.nextInt(Byte.SIZE); // a random bit
+                rbit = 1 << rand.nextInt(Byte.SIZE); // a random bit 
             // Flip the random bit within the random byte:
             corruptBytes[rbyte] = (byte)(corruptBytes[rbyte] ^ rbit);
             sendPkt = new DatagramPacket(corruptBytes,
