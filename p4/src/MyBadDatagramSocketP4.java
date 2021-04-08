73f6240da413d5d009246c3dadce0f31c5afcbfc
HanlonsStraightRazor <muellermartin98@gmail.com>
2021-04-08 12:04:24 -0500

    Retabbed and removed trailing whitespace.

--- p4/src/MyBadDatagramSocketP4.java
+++ p4/src/MyBadDatagramSocketP4.java
@@ -6,7 +6,7 @@ import java.net.SocketException;
 import java.util.Random;
 
 /**
- * Mimic DatagramSocket, but inject errors to test robustness of rdt protocol: 
+ * Mimic DatagramSocket, but inject errors to test robustness of rdt protocol:
  * 1. Drop a packet, or
  * 2. Corrupt one bit.
  * Do not modify any of this code without talking to me first.
@@ -14,30 +14,30 @@ import java.util.Random;
  */
 public class MyBadDatagramSocketP4 extends DatagramSocket {
     private final int DEFAULT_ONE_OUT_OF = 3;
-    private int dropOneOutOf = DEFAULT_ONE_OUT_OF, 
+    private int dropOneOutOf = DEFAULT_ONE_OUT_OF,
                 corruptOneOutOf = DEFAULT_ONE_OUT_OF;
     private Random rand = new Random();
-    
+
     public MyBadDatagramSocketP4() throws SocketException {
         super();
     }
-    
+
     public MyBadDatagramSocketP4(int inPort) throws SocketException {
         super(inPort);
     }
-    
+
     public void setDropOneOutOf(int inDropOneOutOf) {
         if (inDropOneOutOf < 0)
             throw new IllegalArgumentException();
         dropOneOutOf = inDropOneOutOf;
     }
-    
+
     public void setCorruptOneOutOf(int inCorruptOneOutOf) {
         if (inCorruptOneOutOf < 0)
             throw new IllegalArgumentException();
         dropOneOutOf = inCorruptOneOutOf;
     }
-    
+
     /*
     private String firstFewBytesStr(byte[] inArr) {
         String firstBytes = "";
@@ -48,35 +48,35 @@ public class MyBadDatagramSocketP4 extends DatagramSocket {
         return firstBytes;
     }
     */
-   
+
     @Override
     public void send(DatagramPacket inPkt) throws IOException {
         if (dropOneOutOf > 0 && rand.nextInt(dropOneOutOf) == 0) {
             if (inPkt.getLength() > 0) {
                 //String firstBytes = firstFewBytesStr(inPkt.getData());
-                //System.out.println("!!! DROPPED first bytes=" + firstBytes); 
-            }     
+                //System.out.println("!!! DROPPED first bytes=" + firstBytes);
+            }
             else
                 //System.out.println("!!! DROPPED");
             return;
         }
         DatagramPacket sendPkt = inPkt;
-        if (corruptOneOutOf > 0 && 
-            inPkt.getLength() > 0 && 
+        if (corruptOneOutOf > 0 &&
+            inPkt.getLength() > 0 &&
             rand.nextInt(corruptOneOutOf) == 0) {
             int len = inPkt.getLength();
             byte[] corruptBytes = new byte[len],
                    inPktBytes = inPkt.getData();
-            System.arraycopy(inPktBytes, 
-                             0, 
-                             corruptBytes, 
-                             0, 
+            System.arraycopy(inPktBytes,
+                             0,
+                             corruptBytes,
+                             0,
                              len);
             /*
             Flip exactly one random bit...
             */
             int rbyte = rand.nextInt(len), // a random byte
-                rbit = 1 << rand.nextInt(Byte.SIZE); // a random bit 
+                rbit = 1 << rand.nextInt(Byte.SIZE); // a random bit
             // Flip the random bit within the random byte:
             corruptBytes[rbyte] = (byte)(corruptBytes[rbyte] ^ rbit);
             //String firstBytes = firstFewBytesStr(corruptBytes);
--- p4/src/Server.java
+++ p4/src/Server.java
@@ -4,102 +4,102 @@ import java.net.DatagramSocket;
 import java.util.Random;
 
 public class Server {
-	public static final int MSS = Long.BYTES + 2; // Do not modify
-	public static final int ACK_SIZE = 3; // Do not modify
-	public static final int LISTEN_PORT = 9998; // Do not modify
-	public static final byte LOW = 0x01; // Do not modify
-	public static final byte HIGH = 0x02; // Do not modify
-	public static final byte WINNER = 0x03; // Do not modify
-	private static DatagramSocket dgSocket = null; // Do not modify
-	public static void main(String args[]) {
-		// Do not modify any of the code in this method
-		try {
-			Random rand = new Random();
-			System.out.print("SERVER Creating socket...");
-			dgSocket = new MyBadDatagramSocketP4(LISTEN_PORT);
-			System.out.println("good");
-			long secret = rand.nextLong();
-			System.out.println("SERVER Secret=" + secret);
-			DatagramPacket ackPkt = null;
-			byte[] ackData = new byte[ACK_SIZE];
-			while (true) {
-				DatagramPacket rcvPkt = new DatagramPacket(new byte[MSS], MSS);
-				System.out.print("SERVER Waiting for guess...");
-				dgSocket.receive(rcvPkt);
-				ackPkt = new DatagramPacket(ackData, 
-						                    ackData.length, 
-						                    rcvPkt.getAddress(), 
-						                    rcvPkt.getPort());
-				if (packetHasWrongSeqNum(rcvPkt)) {
-					System.out.println("wrong sequence number");
-					makeAckPktNotOK(ackPkt);
-				}
-				else if (packetCorrupt(rcvPkt)) {
-					System.out.println("corrupt");
-					makeAckPktNotOK(ackPkt);
-				}
-				else  {
-					long guess = extractGuess(rcvPkt.getData());
-					System.out.print("got it, guess=" + guess + ", ");
-					byte response = WINNER;
-					if (guess < secret) {
-						response = LOW;
-						System.out.println("too low");
-					}
-					else if (guess > secret) {
-						response = HIGH;
-						System.out.println("too high");
-					}
-					else {
-						System.out.println("winner!");
-					}
-					ackData[1] = response;
-					makeAckPktOK(ackPkt);
-					updateSeqNum();
-				}
-				System.out.print("SERVER Sending response...");
-				dgSocket.send(ackPkt);
-				System.out.println("good");
-			}
-		}
-		catch (IOException ex) {
-			System.out.println("error: " + ex);
-		}
-	}
-	
-	//private static int expSeqNum = ;
-	
-	/*
-	 * Give comment blocks for ALL of the following methods...
-	 */
-	
-	
-	private static void updateSeqNum() {
-		// You can modify only the code inside this method
-	}
-	
-	private static void makeAckPktOK(DatagramPacket inAckPkt) {
-		// You can modify only the code inside this method
-	}
-	
-	private static void makeAckPktNotOK(DatagramPacket inAckPkt) {
-		// You can modify only the code inside this method
-	}
-	
-	private static long extractGuess(byte[] data) throws IOException {
-		// You can modify only the code inside this method
-		return 0;
-	}
-	
-	private static boolean packetHasWrongSeqNum(DatagramPacket inPkt) {
-		// You can modify only the code inside this method
-		return false;
-	}
-	
-	private static boolean packetCorrupt(DatagramPacket inPkt) {
-		// You can modify only the code inside this method
-		return false;
-	}
-	
-	// You may add extra methods...
+    public static final int MSS = Long.BYTES + 2; // Do not modify
+    public static final int ACK_SIZE = 3; // Do not modify
+    public static final int LISTEN_PORT = 9998; // Do not modify
+    public static final byte LOW = 0x01; // Do not modify
+    public static final byte HIGH = 0x02; // Do not modify
+    public static final byte WINNER = 0x03; // Do not modify
+    private static DatagramSocket dgSocket = null; // Do not modify
+    public static void main(String args[]) {
+        // Do not modify any of the code in this method
+        try {
+            Random rand = new Random();
+            System.out.print("SERVER Creating socket...");
+            dgSocket = new MyBadDatagramSocketP4(LISTEN_PORT);
+            System.out.println("good");
+            long secret = rand.nextLong();
+            System.out.println("SERVER Secret=" + secret);
+            DatagramPacket ackPkt = null;
+            byte[] ackData = new byte[ACK_SIZE];
+            while (true) {
+                DatagramPacket rcvPkt = new DatagramPacket(new byte[MSS], MSS);
+                System.out.print("SERVER Waiting for guess...");
+                dgSocket.receive(rcvPkt);
+                ackPkt = new DatagramPacket(ackData,
+                                            ackData.length,
+                                            rcvPkt.getAddress(),
+                                            rcvPkt.getPort());
+                if (packetHasWrongSeqNum(rcvPkt)) {
+                    System.out.println("wrong sequence number");
+                    makeAckPktNotOK(ackPkt);
+                }
+                else if (packetCorrupt(rcvPkt)) {
+                    System.out.println("corrupt");
+                    makeAckPktNotOK(ackPkt);
+                }
+                else  {
+                    long guess = extractGuess(rcvPkt.getData());
+                    System.out.print("got it, guess=" + guess + ", ");
+                    byte response = WINNER;
+                    if (guess < secret) {
+                        response = LOW;
+                        System.out.println("too low");
+                    }
+                    else if (guess > secret) {
+                        response = HIGH;
+                        System.out.println("too high");
+                    }
+                    else {
+                        System.out.println("winner!");
+                    }
+                    ackData[1] = response;
+                    makeAckPktOK(ackPkt);
+                    updateSeqNum();
+                }
+                System.out.print("SERVER Sending response...");
+                dgSocket.send(ackPkt);
+                System.out.println("good");
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("error: " + ex);
+        }
+    }
+
+    //private static int expSeqNum = ;
+
+    /*
+     * Give comment blocks for ALL of the following methods...
+     */
+
+
+    private static void updateSeqNum() {
+        // You can modify only the code inside this method
+    }
+
+    private static void makeAckPktOK(DatagramPacket inAckPkt) {
+        // You can modify only the code inside this method
+    }
+
+    private static void makeAckPktNotOK(DatagramPacket inAckPkt) {
+        // You can modify only the code inside this method
+    }
+
+    private static long extractGuess(byte[] data) throws IOException {
+        // You can modify only the code inside this method
+        return 0;
+    }
+
+    private static boolean packetHasWrongSeqNum(DatagramPacket inPkt) {
+        // You can modify only the code inside this method
+        return false;
+    }
+
+    private static boolean packetCorrupt(DatagramPacket inPkt) {
+        // You can modify only the code inside this method
+        return false;
+    }
+
+    // You may add extra methods...
 }
